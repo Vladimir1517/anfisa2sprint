2842afb4f74f1257bd5c2a82a7be829e49f5f3f5
Владимир Тютин <vladimir30061517@gmail.com>
2023-08-22 16:57:08 +0300

    Admin1

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -9,7 +9,6 @@ class Category(PublishedModel):
     output_order = models.PositiveSmallIntegerField(default=100, verbose_name='Порядок отображения')
 
     class Meta:
-        verbose_name = 'категория'
         verbose_name_plural = 'Категории'
 
     def __str__(self):
@@ -29,10 +28,12 @@ class Topping(PublishedModel):
 
 
 class Wrapper(PublishedModel):
-    title = models.CharField(max_length=256, verbose_name='Название')
+    title = models.CharField(max_length=256,
+                             verbose_name='Название',
+                             help_text='Уникальное название обёртки, не более 256 символов'
+                             )
 
     class Meta:
-        verbose_name = 'обёртка'
         verbose_name_plural = 'Обёртки'
 
     def __str__(self):
@@ -41,21 +42,23 @@ class Wrapper(PublishedModel):
 
 class IceCream(PublishedModel):
     title = models.CharField(max_length=256, verbose_name='Название')
-    description = models.TextField()
+    description = models.TextField(verbose_name='Описание')
     wrapper = models.OneToOneField(
         Wrapper,
         on_delete=models.SET_NULL,
         related_name='ice_cream',
         null=True,
         blank=True,
+        verbose_name='обёртка'
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
         related_name='ice_creams',
+        verbose_name='категория'
     )
     toppings = models.ManyToManyField(Topping)
-    is_on_main = models.BooleanField(default=False)
+    is_on_main = models.BooleanField(default=False, verbose_name = 'На главную')
 
     class Meta:
         verbose_name = 'мороженое'
